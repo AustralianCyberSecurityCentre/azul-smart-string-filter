d9cd4e73102247f9ab74cd3b96799a37ce412ea4
dan-acsc <dan-acsc@users.noreply.github.com>
2026-02-26 23:56:47 +0000

    feat: add logging configuration.

--- azul_smart_string_filter/restapi/filter_strings.py
+++ azul_smart_string_filter/restapi/filter_strings.py
@@ -8,6 +8,7 @@ from pydantic import BaseModel
 from starlette_exporter import PrometheusMiddleware, handle_metrics
 
 from azul_smart_string_filter.lib import SmartStringFilter
+from azul_smart_string_filter.restapi.settings import Settings
 
 
 class FileTypes(str, Enum):
@@ -87,7 +88,8 @@ def is_supported_file_format(file_format, file_format_enum):
 
 def main():
     """Start server."""
-    uvicorn.run(app, host="0.0.0.0", port=8851, log_level="info")  # noqa S104
+    s = Settings()
+    uvicorn.run(app, host="0.0.0.0", port=8851, log_level=s.log_level)  # noqa S104
 
 
 if __name__ == "__main__":
